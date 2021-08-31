80320405b401200854c1859ac0f62ddab85b9ac1
Petr Parolek <petr.parolek@gmail.com>
2021-08-31 20:09:58 +0200

    improvements

--- app/AppModule/presenters/HomepagePresenter.php
+++ app/AppModule/presenters/HomepagePresenter.php
@@ -3,28 +3,7 @@ declare(strict_types=1);
 
 namespace App\AppModule\Presenters;
 
-use Nette\Http\Response;
-use Nette\Utils\ArrayHash;
-
 class HomepagePresenter extends BasePresenter
 {
 
-	public function renderDefault(): void
-	{
-		$alerts[] = [
-			'message' => 'Nastavení aplikace není v pořádku',
-			'type' => 'danger',
-		];
-
-		$alerts = ArrayHash::from($alerts);
-
-		$this->template->appError = true;
-
-		$httpResponse = $this->getHttpResponse();
-		$httpResponse->setCode(Response::S403_FORBIDDEN);
-
-		foreach ($alerts as $alert) {
-			$this->flashMessage($alert->message, $alert->type);
-		}
-	}
 }
